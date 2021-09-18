58868cdf87a4ffe8593888c955d059514e7686e5
ihsan-guc <ihsanguc.33@gmail.com>
2021-09-18 21:43:30 +0300

    Add Entites Layer

---
+++ ElasticSearchExample.Entites/Entities/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ElasticSearchExample.Entites.Entities
+{
+    public interface IEntity<T>
+    {
+        T Id { get; set; }
+    }
+    public class BaseEntity<T> : IEntity<T>
+    {
+        public T Id { get; set; }
+    }
+    public class BaseGuidEntity : BaseEntity<int>
+    {
+
+    }
+    public class TrackableEntity : BaseGuidEntity
+    {
+        public Guid CreaterId { get; set; }
+        public Guid? LastUpdaterId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? LastUpdateDate { get; set; }
+    }
+}
---
+++ ElasticSearchExample.Entites/Entities/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace ElasticSearchExample.Entites.Entities
+{
+    public class User : BaseGuidEntity
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
