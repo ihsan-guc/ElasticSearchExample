df7507d49deb0c8f74193144779ca15b5579f325
ihsan-guc <ihsanguc.33@gmail.com>
2021-12-19 11:16:27 +0300

    Rabbit.MQ Sender.

---
+++ RabbitMQ.Sender/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var mainQueue = "RabbitMQ_" + "Animals";
+            var mainQueue2 = "RabbitMQ_" + "Animals" + "_2";
+            var mainQueue3 = "RabbitMQ_" + "Animals" + "_3";
+            string[] jsonAnimals = { "Aardvark", "Albatross", "Alligator", "Alpaca", "Ant", "Anteater", "Antelope", "Ape", "Armadillo", "Donkey", "Baboon", "Badger", "Barracuda", "Bat", "Bear", "Beaver", "Bee", "Bison", "Boar", "Buffalo", "Butterfly", "Camel", "Capybara", "Caribou", "Cassowary", "Cat", "Caterpillar", "Cattle", "Chamois", "Cheetah", "Chicken", "Chimpanzee", "Chinchilla", "Chough", "Clam", "Cobra", "Cockroach", "Cod", "Cormorant", "Coyote", "Crab", "Crane", "Crocodile", "Crow", "Curlew", "Deer", "Dinosaur", "Dog", "Dogfish", "Dolphin", "Dotterel", "Dove", "Dragonfly", "Duck", "Dugong", "Dunlin", "Eagle", "Echidna", "Eel", "Eland", "Elephant", "Elk", "Emu", "Falcon", "Ferret", "Finch", "Fish", "Flamingo", "Fly", "Fox", "Frog", "Gaur", "Gazelle", "Gerbil", "Giraffe", "Gnat", "Gnu", "Goat", "Goldfinch", "Goldfish", "Goose", "Gorilla", "Goshawk", "Grasshopper", "Grouse", "Guanaco", "Gull", "Hamster", "Hare", "Hawk", "Hedgehog", "Heron", "Herring", "Hippopotamus", "Hornet", "Horse", "Human", "Hummingbird", "Hyena", "Ibex", "Ibis", "Jackal", "Jaguar", "Jay", "Jellyfish", "Kangaroo", "Kingfisher", "Koala", "Kookabura", "Kouprey", "Kudu", "Lapwing", "Lark", "Lemur", "Leopard", "Lion", "Llama", "Lobster", "Locust", "Loris", "Louse", "Lyrebird", "Magpie", "Mallard", "Manatee", "Mandrill", "Mantis", "Marten", "Meerkat", "Mink", "Mole", "Mongoose", "Monkey", "Moose", "Mosquito", "Mouse", "Mule", "Narwhal", "Newt", "Nightingale", "Octopus", "Okapi", "Opossum", "Oryx", "Ostrich", "Otter", "Owl", "Oyster", "Panther", "Parrot", "Partridge", "Peafowl", "Pelican", "Penguin", "Pheasant", "Pig", "Pigeon", "Pony", "Porcupine", "Porpoise", "Quail", "Quelea", "Quetzal", "Rabbit", "Raccoon", "Rail", "Ram", "Rat", "Raven", "Red deer", "Red panda", "Reindeer", "Rhinoceros", "Rook", "Salamander", "Salmon", "Sand Dollar", "Sandpiper", "Sardine", "Scorpion", "Seahorse", "Seal", "Shark", "Sheep", "Shrew", "Skunk", "Snail", "Snake", "Sparrow", "Spider", "Spoonbill", "Squid", "Squirrel", "Starling", "Stingray", "Stinkbug", "Stork", "Swallow", "Swan", "Tapir", "Tarsier", "Termite", "Tiger", "Toad", "Trout", "Turkey", "Turtle", "Viper", "Vulture", "Wallaby", "Walrus", "Wasp", "Weasel", "Whale", "Wildcat", "Wolf", "Wolverine", "Wombat", "Woodcock", "Woodpecker", "Worm", "Wren", "Yak", "Zebra" };
+
+            var queueModel = new List<string>(jsonAnimals);
+
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName  ="guest", Password= "guest" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: mainQueue);
+                channel.QueueDeclare(queue: mainQueue2);
+                channel.QueueDeclare(queue: mainQueue3);
+
+                foreach (var queueItem in queueModel)
+                {
+                    string message = JsonConvert.SerializeObject(queueItem);
+                    var body = Encoding.UTF8.GetBytes(message);
+                    if (channel.MessageCount(mainQueue) < 20)
+                    {
+                        channel.BasicPublish(exchange: "",routingKey: mainQueue,basicProperties: null,body: body);
+                    }
+                    else if (channel.MessageCount(mainQueue2) < 20)
+                    {
+                        channel.BasicPublish(exchange: "", routingKey: mainQueue2, basicProperties: null, body: body);
+                    }
+                    else
+                    {
+                        channel.BasicPublish(exchange: "", routingKey: mainQueue3, basicProperties: null, body: body);
+                    }
+                    Console.WriteLine(message);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    } 
+}
