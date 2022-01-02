cc2198976b5b37549570c3c169845385b5f99c31
ihsan-guc <ihsanguc.33@gmail.com>
2022-01-02 12:27:26 +0300

    Benchmark testting created.

--- BenchmarkConsole/Program.cs
+++ BenchmarkConsole/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using ElasticSearchExample.Web.Core;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BenchmarkConsole
 {
@@ -6,7 +12,37 @@ namespace BenchmarkConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var result = BenchmarkRunner.Run<BenchMarkDemo>();
+            Console.WriteLine("Bencmark Testting.!");
+        }
+    }
+    public class BenchMarkDemo
+    {
+        [Benchmark]
+        public List<ElasticSearchViewModel> ElasticSearchFuzzinessOneRatio()
+        {
+            var value = "Eura";
+            var elasticNode = new ElasticSearchHelper();
+            var client = elasticNode.ElasticClientNode;
+            var dataList = client.Search<ElasticSearchViewModel>(s =>
+            s.Query(q => q.Bool(b => b.Should(sh => sh.Fuzzy(f => f.Field(fi => fi.FullName).Fuzziness(Fuzziness.EditDistance(1)).Boost(2)
+                   .Value(value))
+            , m => m.Match(mq => mq.Field(f => f.FullName).Query(value).Operator(Operator.And).Fuzziness(Fuzziness.EditDistance(1))))
+            )).Size(10));
+            return dataList.Documents.ToList();
+        }
+        [Benchmark]
+        public List<ElasticSearchViewModel> ElasticSearchFuzzinessTwoRatio()
+        {
+            var value = "Eura";
+            var elasticNode = new ElasticSearchHelper();
+            var client = elasticNode.ElasticClientNode;
+            var dataList = client.Search<ElasticSearchViewModel>(s =>
+            s.Query(q => q.Bool(b => b.Should(sh => sh.Fuzzy(f => f.Field(fi => fi.FullName).Fuzziness(Fuzziness.EditDistance(2)).Boost(2)
+                   .Value(value))
+            , m => m.Match(mq => mq.Field(f => f.FullName).Query(value).Operator(Operator.And).Fuzziness(Fuzziness.EditDistance(2))))
+            )).Size(10));
+            return dataList.Documents.ToList();
         }
     }
 }
