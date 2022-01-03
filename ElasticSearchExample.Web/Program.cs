639f9a8c8fdae8ed56189916c2a2afe374cbeef6
ihsan-guc <ihsanguc.33@gmail.com>
2022-01-03 11:26:43 +0300

    Background tasks with hosted services in ASP.NET Core

--- ElasticSearchExample.Web/Program.cs
+++ ElasticSearchExample.Web/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ElasticSearchExample.Web
@@ -21,6 +23,39 @@ namespace ElasticSearchExample.Web
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                }).ConfigureServices(services => services.AddHostedService<ElasticSearchCreateIndexService>());
+    }
+    public class ElasticSearchCreateIndexService : IHostedService, IDisposable
+    {
+        private int executionCount = 0;
+        private Timer _timer = null!;
+
+        public ElasticSearchCreateIndexService()
+        {
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object? state)
+        {
+            var count = Interlocked.Increment(ref executionCount);
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
     }
 }
